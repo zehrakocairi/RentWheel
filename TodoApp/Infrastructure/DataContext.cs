f0bf0905b0cbd2a423d0126e06c3009533318ad0
zehra <zehrakocairi@gmail.com>
2024-05-15 23:58:33 +0200

    add new tables to context

--- TodoApp/Infrastructure/DataContext.cs
+++ TodoApp/Infrastructure/DataContext.cs
@@ -12,4 +12,12 @@ public class DataContext : DbContext
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
     
     public DbSet<Product> Products { get; set; } = null!;
+
+    public DbSet<Company> Companies { get; set; }= null!;
+
+    public DbSet<Car> Cars { get; set; }= null!;
+
+    public DbSet<Client> Clients { get; set; }= null!;
+
+    public DbSet<Rent> Rents { get; set; }= null!;
 }
